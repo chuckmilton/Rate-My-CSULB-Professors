0209b51df0f2ef48c87a5865253e49286755fcdd
Charles Milton <139507918+chuckmilton@users.noreply.github.com>
2024-12-05 16:40:54 -0800

    Correct naming

--- api/graphql.js
+++ api/graphql.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": process.env.AUTHORIZATION_TOKEN, // Add the Bearer token
+          "Authorization": AUTHORIZATION_TOKEN, // Add the Bearer token
           "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
         },
